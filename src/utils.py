6c92bec3d3e6cbbb14165f36f25bc9c4cf2ee612
naveen kumar <naveen8296088066@gmail.com>
2026-01-13 13:44:30 +0530

    done hyperparameter tunning'

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 from src.logger import logging
 from src.exception import CustomException
 import dill
-
+from sklearn.metrics import r2_score
 
 def save_object(file_path,obj):
     try:
@@ -18,3 +18,39 @@ def save_object(file_path,obj):
 
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+    import sys
+from sklearn.metrics import r2_score
+from src.exception import CustomException
+from src.logger import logging
+
+
+def evaluate_models(x_train, y_train, x_test, y_test, models):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            logging.info(f"Training model: {model_name}")
+
+            model.fit(x_train, y_train)
+
+            y_train_pred = model.predict(x_train)
+            y_test_pred = model.predict(x_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+            logging.info(
+                f"{model_name} -> Train R2: {train_model_score}, Test R2: {test_model_score}"
+            )
+
+        return report
+
+    except Exception as e:
+        logging.error("Exception occurred in evaluate_models")
+        raise CustomException(e, sys)
+
+    
